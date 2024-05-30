21ea17526181f86b0c32f0bfa963d944a1d91e40
Binziya01 <binziyasherin05@gmail.com>
2024-05-30 12:34:56 +0530

    changed file

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
   }
   return (
     <div className="flex bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen justify-center flex-col items-center">
-      <h1 className="text-6xl font-semibold text-pink-700">Generate an excuse</h1>
+      <h1 className="text-4xl font-semibold text-pink-700">Generate an excuse</h1>
       <div className="flex gap-6 flex-col pt-16">
       <button onClick={()=>{fetchData("funny")}} className="bg-yellow-300 px-7 py-3 rounded-md text-xl">Funny</button>
       <button onClick={()=>{fetchData("developers")}} className="bg-orange-700 px-7 py-3 rounded-md text-xl">Developer</button>
       <button onClick={()=>{fetchData("office")}} className="bg-green-700 px-7 py-3 rounded-md text-xl">Office</button>
       </div>
       
-      <h1 className="pt-10 text-xl">{excuse}</h1>
+      <h1 className="pt-10 text-xl px-6">{excuse}</h1>
     </div>
   );
 }
